07f159ce402172c6fca4f4e0d922b1c471ed1c6d
unknown <maykaren20ram@gmail.com>
2019-10-20 23:35:03 -0500

    Se añadio sopa-letrasv1

---
+++ sopa-letrasCopia/js/configRecurso.js
@@ -0,0 +1,11 @@
+var ambSCORM =false;
+var barraSCORM = false;
+
+var retroCal1 = [
+	{LimInf: 0, LimSup: 3, Mensaje: "No fue suficiente"},	
+	{LimInf: 4, LimSup: 7, Mensaje: "Esfuérzate más"},	
+	{LimInf: 8, LimSup: 12, Mensaje: "Suficiente"},	
+	{LimInf: 13, LimSup: 13, Mensaje: "Excelente"},	
+    ];
+    //para retros en inglès
+  
