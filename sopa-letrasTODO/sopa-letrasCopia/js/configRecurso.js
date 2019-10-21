5ce3b365314f14083e4e55f25e6298078425f9f7
unknown <maykaren20ram@gmail.com>
2019-10-20 23:36:54 -0500

    Sopa-letrasv1.1: Contiene respuestas para palabras o parrafos, además del tiempo

---
+++ sopa-letrasTODO/sopa-letrasCopia/js/configRecurso.js
@@ -0,0 +1,12 @@
+var ambSCORM =false;
+var barraSCORM = false;
+var esParrafo = false;
+var debug = true;
+var retroCal1 = [
+	{LimInf: 0, LimSup: 3, Mensaje: "No fue suficiente"},	
+	{LimInf: 4, LimSup: 7, Mensaje: "Esfuérzate más"},	
+	{LimInf: 8, LimSup: 12, Mensaje: "Suficiente"},	
+	{LimInf: 13, LimSup: 13, Mensaje: "Excelente"},	
+    ];
+    //para retros en inglès
+  
